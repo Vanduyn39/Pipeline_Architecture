b7b703ffd86b6d76616d360c97894674574d2fb6
Vanduyn39 <phanthivanduyen@gmail.com>
2025-03-05 22:19:37 +0700

    add interface

--- src/main/java/org/company/pipeline_architecture/controller/HomeController.java
+++ src/main/java/org/company/pipeline_architecture/controller/HomeController.java
@@ -1,18 +1,23 @@
 package org.company.pipeline_architecture.controller;
 
+import org.company.pipeline_architecture.core.CreditNote;
+import org.company.pipeline_architecture.core.Payment;
+import org.company.pipeline_architecture.core.PaymentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.company.pipeline_architecture.core.Payment;
-import org.company.pipeline_architecture.core.CreditNote;
-import org.company.pipeline_architecture.core.PaymentService;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Chào mừng đến với Spring Boot JSP!");
-        return "home"; // Điều hướng đến home.jsp
+        
+        return "Customer"; // Trả về file index.html trong thư mục templates
+    }
+    @GetMapping("/order")
+    public String order(Model model) {
+        
+        return "Order"; // Trả về file index.html trong thư mục templates
     }
     private PaymentService paymentService = new PaymentService();
 
